071a8e3e9d41f7142ea18c2e49a7244b9d7303c0
vinothsmart <s.vinothkanna90@gmail.com>
2021-02-08 13:19:29 +0530

    2.7 flex and layout added

--- App.js
+++ App.js
@@ -5,9 +5,20 @@ import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 export default function App() {
   return (
     <View style={{ padding: 50 }}>
-      <View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <TextInput
-          style={{ borderColor: "black", borderWidth: 1, padding: 10 }}
+          style={{
+            width: "80%",
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10,
+          }}
           placeholder="Course Goal"
         />
         <Button title="Add" />
