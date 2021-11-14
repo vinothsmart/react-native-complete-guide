dbcf0aa888e84e92ff528ff57550a898e834d34b
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-14 07:37:12 +0530

    inline styles added

--- App.js
+++ App.js
@@ -4,37 +4,24 @@ import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: "row",
-        width: "80%",
-        height: 300,
-        justifyContent: "space-around",
-        alignItems: "stretch",
-      }}
-    >
-      {/* <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          style={{
-            width: "80%",
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-          }}
-          placeholder="Course Goal"
-        />
+    // <View
+    //   style={{
+    //     padding: 50,
+    //     flexDirection: "row",
+    //     width: "80%",
+    //     height: 300,
+    //     justifyContent: "space-around",
+    //     alignItems: "stretch",
+    //   }}
+    // >
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Course Goal" />
         <Button title="Add" />
       </View>
       <View></View>
-      <StatusBar style="auto" /> */}
-      <View
+      <StatusBar style="auto" />
+      {/* <View
         style={{
           backgroundColor: "red",
           flex: 1,
@@ -63,9 +50,24 @@ export default function App() {
         }}
       >
         <Text>3</Text>
-      </View>
+      </View> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+});
