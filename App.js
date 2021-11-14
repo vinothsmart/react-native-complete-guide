618f90d0ee6e94ed3c68113c26c072cab19dbb05
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-14 07:38:09 +0530

    rm unwanted code

--- App.js
+++ App.js
@@ -4,53 +4,13 @@ import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
   return (
-    // <View
-    //   style={{
-    //     padding: 50,
-    //     flexDirection: "row",
-    //     width: "80%",
-    //     height: 300,
-    //     justifyContent: "space-around",
-    //     alignItems: "stretch",
-    //   }}
-    // >
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput style={styles.input} placeholder="Course Goal" />
         <Button title="Add" />
       </View>
-      <View></View>
+      <View />
       <StatusBar style="auto" />
-      {/* <View
-        style={{
-          backgroundColor: "red",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>1</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: "blue",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: "green",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>3</Text>
-      </View> */}
     </View>
   );
 }
