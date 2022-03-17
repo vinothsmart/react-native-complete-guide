e09c1174b5255d0d2d42f8c4bf1fc6dc2bd1912e
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-17 16:09:50 +0530

    title updated

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useState } from "react/cjs/react.development";
 
 export default function App() {
+  const [outputText, setOutputText] = useState(
+    "Open up App.js to start working on your app!"
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button
+        title="Change Text"
+        onPress={() => setOutputText("The text changed")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
